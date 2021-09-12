698a41bdf2365de548f6e4f9be51a884a5140d31
sharathp <chandra.ps@gmail.com>
2021-09-12 15:52:14 -0400

    Updated trial-service to use opentracing instead of opentelemetry wiring to be able to capture/export prometheus metrics.

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/trial/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/trial/app.py
@@ -1,34 +1,62 @@
+import logging
+
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-
-from opentelemetry import trace
-from opentelemetry.exporter import jaeger
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.trace import TracerProvider
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+# from opentelemetry import trace
+# from opentelemetry.exporter import jaeger
+# from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+# from opentelemetry.instrumentation.flask import FlaskInstrumentor
+# from opentelemetry.instrumentation.requests import RequestsInstrumentor
+# from opentelemetry.sdk.trace import TracerProvider
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+from flask_opentracing import FlaskTracing
+from requests_opentracing import SessionTracing
 
-# Export OpenTelemetry Traces/Spans to Jaeger
-trace.set_tracer_provider(TracerProvider())
-jaeger_exporter = jaeger.JaegerSpanExporter(service_name='nd064course4-trial-service')
-trace.get_tracer_provider().add_span_processor(
-    BatchExportSpanProcessor(jaeger_exporter)
-)
 
 app = Flask(__name__)
 metrics = GunicornInternalPrometheusMetrics(app)
 CORS(app)
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
+# FlaskInstrumentor().instrument_app(app)
+# RequestsInstrumentor().instrument()
+
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(service_name_label=service)
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+
+jaeger_tracer = init_tracer('nd064course4-trial-service')
+# only trace requests explicitly annotated to be traced, this will prevent tracing "non-functional" requests
+# such as prometheus /metrics
+tracing = FlaskTracing(jaeger_tracer, False, app)
 
 
 @app.route('/')
+@tracing.trace()
 def homepage():
     return render_template("main.html")
     with tracer.start_span('get-python-jobs') as span:
         homepages = []
-        res = requests.get('https://jobs.github.com/positions.json?description=python')
+        traced_session = SessionTracing(jaeger_tracer)
+        res = traced_session.get('https://jobs.github.com/positions.json?description=python')
         span.set_tag('first-tag', len(res.json()))
         for result in res.json():
             try:
