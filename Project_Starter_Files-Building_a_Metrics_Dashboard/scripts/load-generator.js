53887ab03103e9125b373f4fab0671973b235557
sharathp <chandra.ps@gmail.com>
2021-09-13 21:46:01 -0400

    Added Grafana dashboard.  Extracted ServiceMonitor definitions from app manifests into its own file.

--- Project_Starter_Files-Building_a_Metrics_Dashboard/scripts/load-generator.js
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/scripts/load-generator.js
@@ -1,5 +1,6 @@
 import { sleep } from "k6";
 import http from "k6/http";
+import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.1.0/index.js";
 
 export let options = {
   vus: 10,
@@ -10,8 +11,10 @@ export default function main() {
   let response;
 
   // Trial-Service
-  response = http.get("http://localhost:8083/");
-  sleep(1);
+  if(randomIntBetween(1, 25) == 13) {
+    response = http.get("http://localhost:8083/");
+    sleep(1);
+  }
 
   // Backend-Service:Home
   response = http.get("http://localhost:8081/");
@@ -24,4 +27,10 @@ export default function main() {
   // Backend-Service:API
   response = http.get("http://localhost:8081/api");
   sleep(1);
+
+  // Backend-Service:Star
+  if(randomIntBetween(1, 10) == 5) {
+    response = http.get("http://localhost:8081/star");
+    sleep(1);
+  }
 }
